4cf8a0280ad11750a4de5aa8363bee478fe1bd44
willigu <willigu@zedat.fu-berlin.de>
2014-11-08 18:09:29 +0100

    Change sprites while move

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    private Transform objPlayer;
+    public float TriggerDistance = 5;
+    public float Velocity = 2.5f;
+	// Use this for initialization
+	void Start () {
+        this.objPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(this.objPlayer);
+        if (Vector2.Distance(this.transform.position, this.objPlayer.transform.position) <= this.TriggerDistance)
+        {
+            Debug.Log("Player has been seen");
+            // attack player
+        }
+        else
+        {
+            this.transform.position += this.transform.forward * this.Velocity * Time.deltaTime;
+        }
+	}
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,28 +2,37 @@
 using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
+    public Sprite leftSprite, rightSprite, upSprite;
+
+    private Sprite downSprite;
+    private SpriteRenderer sprRenderer;
 
 	// Use this for initialization
 	void Start () {
-	
+        this.sprRenderer = GetComponent<SpriteRenderer>();
+        this.downSprite = this.sprRenderer.sprite;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            this.sprRenderer.sprite = this.downSprite;
             this.transform.Translate(new Vector3(0f, -1f));
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            this.sprRenderer.sprite = this.upSprite;
             this.transform.Translate(new Vector3(0f, 1f));
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            this.sprRenderer.sprite = this.leftSprite;
             this.transform.Translate(new Vector3(-1f, 0f));
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            this.sprRenderer.sprite = this.rightSprite;
             this.transform.Translate(new Vector3(1f, 0f));
         }
 
