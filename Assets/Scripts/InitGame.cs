a4e3f214c54e16161db1b594583463c2bc93b004
willigu <willigu@zedat.fu-berlin.de>
2014-11-08 16:48:47 +0100

    First Commit

---
+++ Assets/Scripts/InitGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitGame : MonoBehaviour {
+    
+    GameObject[,] arrayField = new GameObject[64,64];
+    GameObject objDummy = new GameObject();
+
+    public GameObject brickWall;
+	// Use this for initialization
+    public void Awake()
+    {
+           // dot tween
+        for (int i = 0; i < 64; i++){
+            for (int j = 0; j < 64; j++){
+                if (Random.Range(0, 2) == 1)
+                {
+                    arrayField[i, j] = Instantiate(brickWall, new Vector2(i, j), this.transform.rotation) as GameObject;
+                }
+                else
+                {
+                    arrayField[i, j] = objDummy;
+                }
+                
+            }
+        }
+    }
+    void Start()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.Translate(new Vector3(0f, -1.28f));
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.transform.Translate(new Vector3(0f, 1.28f));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(new Vector3(-1.28f, 0f));
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.Translate(new Vector3(1.28f, 0f));
+        }
+
+	}
+}
